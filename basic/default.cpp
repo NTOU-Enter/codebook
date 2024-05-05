d1fdd9171f760a7bc8c6d29424648d7d90e3e0f1
owen0806 <owen080614@gmail.com>
2024-05-05 22:20:01 +0800

    Initial commit

---
+++ basic/default.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define int long long
+#define ld long double
+#define endl '\n'
+#define PB push_back
+#define pii pair<int, int>
+#define SZ(v) (int)v.size()
+#define all(v) v.begin(), v.end()
+#define ff first
+#define ss second
+#define PI acos(-1)
+using namespace std;
+
+void solve(){
+}
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0);
+
+    int T = 1;
+    // cin >> T;
+    while(T--){
+        solve();
+    }
+}
---
+++ basic/gen.py
@@ -0,0 +1,8 @@
+from random import *
+
+n = randint(1, 10)
+print(n)
+
+for i in range(n):
+    ai = randint(1, 10)
+    print(ai, end=' ')
